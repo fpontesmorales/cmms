a80093177d0cf9e88f12547abb1b6474e21b9fbe
Felipe Morales <fpontesmorales@gmail.com>
2025-07-03 11:52:38 -0300

    Configura o envio de e-mail com o Gmail

--- config/settings.py
+++ config/settings.py
@@ -2,17 +2,27 @@ import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-local-e-insegura-para-testes')
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-_2a7r)ix4wllaq3fmhtzz#f6ia%-o0ov7%n#8vn%81o5zxr@qa')
+
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-ALLOWED_HOSTS = ['127.0.0.1', '10.10.2.88']
-if 'PYTHONANYWHERE_DOMAIN' in os.environ:
-    ALLOWED_HOSTS.append(os.environ['PYTHONANYWHERE_DOMAIN'])
+
+ALLOWED_HOSTS = [
+    '127.0.0.1', 
+    '10.10.2.88', 
+    'fpontesmorales.pythonanywhere.com'
+]
 
 INSTALLED_APPS = [
-    'jazzmin', 'django.contrib.admin', 'django.contrib.auth',
-    'django.contrib.contenttypes', 'django.contrib.sessions',
-    'django.contrib.messages', 'django.contrib.staticfiles',
-    'chamados.apps.ChamadosConfig', 'cadastros.apps.CadastrosConfig',
+    'jazzmin',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'chamados.apps.ChamadosConfig',
+    'cadastros.apps.CadastrosConfig',
     'inventario.apps.InventarioConfig',
 ]
 
@@ -29,12 +39,29 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
-    {'BACKEND': 'django.template.backends.django.DjangoTemplates', 'DIRS': [os.path.join(BASE_DIR, 'templates')], 'APP_DIRS': True, 'OPTIONS': {'context_processors': ['django.template.context_processors.debug', 'django.template.context_processors.request', 'django.contrib.auth.context_processors.auth', 'django.contrib.messages.context_processors.messages']}},
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3'}}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
@@ -50,13 +77,18 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 JAZZMIN_SETTINGS = {
-    "site_title": "CMMS INFRA", "site_header": "CMMS IFCE", "site_brand": "INFRA-IFCE",
-    "theme": "flatly", "order_with_respect_to": ["auth", "chamados", "inventario", "cadastros"],
+    "site_title": "CMMS INFRA",
+    "site_header": "CMMS IFCE",
+    "site_brand": "INFRA-IFCE",
+    "theme": "flatly",
+    "order_with_respect_to": ["auth", "chamados", "inventario", "cadastros"],
     "apps": {
         "chamados": {"label": "Gestão de Chamados", "icon": "fas fa-bullhorn"},
         "inventario": {"label": "Inventário", "icon": "fas fa-box-open"},
@@ -77,9 +109,6 @@ LOGIN_REDIRECT_URL = '/painel/'
 LOGIN_URL = '/accounts/login/'
 LOGOUT_REDIRECT_URL = '/'
 
-# ==========================================================
-# CONFIGURAÇÃO DE E-MAIL (MODO DE PRODUÇÃO COM GMAIL)
-# ==========================================================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
